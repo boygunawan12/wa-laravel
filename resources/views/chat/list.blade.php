7fdf8fc69402450fdbd4b483f7da55bac3aa0b9e
faris <fariswidhiarta123@gmail.com>
2020-09-26 11:59:23 +0700

    x

--- resources/views/chat/list.blade.php
+++ resources/views/chat/list.blade.php
@@ -189,7 +189,7 @@ $(".device option").filter(function() {
 
 
 
-Echo.private('on-message-'+window.Laravel.user).listen('OnMessage', function(e) {
+Echo.private('on-message-'+{{user()->id}}).listen('OnMessage', function(e) {
     // console.log("Wena!, a "+e.data.user_name + " le ha gustado uno de tus aportes");
     // console.log(e);
     console.log("x");
